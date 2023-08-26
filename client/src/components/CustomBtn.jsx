ffc5b87ae739ce48cb4bd51ac90257d385e6a4e3
Adee99 <66461199+Adee99@users.noreply.github.com>
2023-08-26 23:11:17 +0530

    update : responsive navbar

--- client/src/components/CustomBtn.jsx
+++ client/src/components/CustomBtn.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const CustomBtn = () => {
+const CustomBtn = ({ btnType, title, handleClick, styles }) => {
   return (
-    <div>CustomBtn</div>
+    <button
+      type={btnType}
+      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+      onClick={handleClick}>
+      {title}
+    </button>
   )
 }
 
